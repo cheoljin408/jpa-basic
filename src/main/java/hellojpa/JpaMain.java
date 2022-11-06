41a0f011ddb697dd9b076320f7155d1965a222db
cheoljin408 <kcj409@naver.com>
2022-11-06 23:26:51 +0900

    설정 변경

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,9 +1,6 @@
 package hellojpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 import java.util.List;
 
 public class JpaMain {
@@ -14,16 +11,17 @@ public class JpaMain {
         EntityManager em = emf.createEntityManager();
 
         EntityTransaction tx = em.getTransaction();
-
         tx.begin();
+
         try {
-            // 영속
-            Member member = new Member(200L, "cheoljin");
-            em.persist(member);
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("hello A");
 
-            em.flush();
+            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
 
-            System.out.println("===================");
+            for (Member member : result) {
+                System.out.println("member.getName() = " + member.getName());
+            }
 
             tx.commit();
         } catch (Exception e) {
@@ -31,6 +29,8 @@ public class JpaMain {
         } finally {
             em.close();
         }
+
+        em.close();
         emf.close();
     }
 }
