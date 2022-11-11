a70632e3fc7c0406efd885f47104e668d685655a
cheoljin408 <kcj409@naver.com>
2022-11-11 15:08:25 +0900

    엔티티 매핑
    - 객체와 테이블 매핑
    - 데이터베이스 스키마 자동 생성
    - 필드와 컬럼 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,7 +1,9 @@
 package hellojpa;
 
-import javax.persistence.*;
-import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
 public class JpaMain {
 
@@ -14,14 +16,7 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Member findMember = em.find(Member.class, 1L);
-//            findMember.setName("hello A");
-
-            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
-
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
+            System.out.println("==================");
 
             tx.commit();
         } catch (Exception e) {
