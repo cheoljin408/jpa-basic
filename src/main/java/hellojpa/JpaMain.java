116333a966c851779b67897e7dd069ba7774d219
cheoljin408 <kcj409@naver.com>
2022-11-21 22:30:32 +0900

    연관관계 매핑 기초
    - 양방향 연관관계와 연관관계의 주인 2 - 주의점, 정리

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -23,8 +23,9 @@ public class JpaMain {
 
             Member member = new Member();
             member.setUsername("member1");
-            member.setTeam(team);
             em.persist(member);
+
+            team.addMember(member);
 
             em.flush();
             em.clear();
